fcb7f2435be7ffac53a1e41e841136ff86ad8317
Hacker_Ujjawal <144308009+HackerUjjawal@users.noreply.github.com>
2024-03-18 15:15:36 +0530

    Api1.py

--- pythonsandbox_u12113_wlvw6i6CSipZ9yxFZxpFuxIO_v4(1).py
+++ pythonsandbox_u12113_wlvw6i6CSipZ9yxFZxpFuxIO_v4(1).py
@@ -1,11 +1,24 @@
-# The answer to this challenge is by using the ...
-# ... filename_prefix="" with the f-string interpolation
-# ... by adding +1 to the index value followed by a space
-# ... the file name becomes: 1 Kingdom Hearts...
-YouTube(playlist[i]).streams.filter(subtype="mp4").first()
-.download(
-  output_path=downloads_dir, 
-  filename_prefix=f"{i+1} "
-)
- 
+import requests
+import json
 
+url = "https://customer-analytics-34146.my.salesforce-sites.com/services/apexrest/createAccount"
+headers = {"Content-Type": "application/json"}
+
+# Replace with your details
+data = {
+    "name": "Your Full Name",
+    "email": "your_colle@example.com",
+    "rollNumber": 2110991923,  
+    "phone": 8709403012  
+}
+
+response = requests.post(url, headers=headers, json=data)
+
+if response.status_code == 200:
+    # Account creation successful
+    data = response.json()
+    account_number = data["accountNumber"]
+    print("Investment account created successfully!")
+    print(f"Your account number: {account_number}")
+else:
+    print("Error creating account:", response.text)
